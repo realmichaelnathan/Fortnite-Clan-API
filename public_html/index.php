d5c6e6d33560582a077201c9e38e3ef2c9fa9663
Michael Nathan <realmichaelnathan@protonmail.com>
2020-01-01 11:49:43 -0600

    moved index file up a level.

--- public_html/index.php
+++ public_html/index.php
@@ -11,7 +11,7 @@
 |
 */
 
-$app = require __DIR__.'/../../api/bootstrap/app.php';
+$app = require __DIR__.'/../api/bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
